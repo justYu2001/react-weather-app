310b3a56e732dcae189c69d1bcf964d3fb83740c
justYu2001 <jack4781012@gmail.com>
2023-01-12 20:52:55 +0800

    factor: `WeekForecast` Component

--- src/components/WeekForecast.jsx
+++ src/components/WeekForecast.jsx
@@ -29,55 +29,55 @@ const DayForecast = tw.li`
 const WeekForecast = (props) => {
     const { weatherData } = props;
 
-    const getChineseDay = (datetimeString) => {
-        const date = new Date(datetimeString.replace(' ', 'T'));
-        const day = date.getDay();
-
-        const chineseDay = ['日', '一', '二', '三', '四', '五', '六'];
-        return `星期${chineseDay[day]}`;
-    };
-
-    const EverydayForecast = () => {
-        if(weatherData.length === 0) {
-            return <li />;
-        }
-        
-        const neededProperties = ['Wx', 'MinT', 'MaxT'];
-        const weekWeatherForecast = weatherData.reduce((neededWeatherData, weatherElement) => {
-            const propertyName = weatherElement.elementName;
-            
-            if(neededProperties.includes(propertyName)) {
-                const neededTime = weatherElement.time.filter((data, index) => {
-                    return index === 0 || data.startTime.includes('06:');
-                });
-
-                neededTime.forEach((data, index) => {
-                    if(index === 7) {
-                        return;
-                    }
-
-                    neededWeatherData[index].day = index === 0 ? '今天' : getChineseDay(data.startTime);
-                    neededWeatherData[index][propertyName] = data.elementValue[0].value;
-                });
-            }
-            
-            return neededWeatherData;
-        }, Array.from({ length: 7 }, () => ({})));
-
-        return weekWeatherForecast.map((weatherForecast) => (
-            <DayForecast key={weatherForecast.day}>
-                <h5 className='flex-grow md:flex-grow-0 md:py-1 font-bold'>{weatherForecast.day}</h5>
-                <WeatherIcon weather={weatherForecast['Wx']} />
-                <p className='w-18 ml-2 md:py-1'>{weatherForecast['MinT']}° - {weatherForecast['MaxT']}°</p>
-            </DayForecast>
-        ));
-    }
-
     return (
         <Container>
-            <EverydayForecast />
+            <EverydayForecast weatherData={weatherData} />
         </Container>
     );
 };
 
 export default WeekForecast;
+
+const getChineseDay = (datetimeString) => {
+    const date = new Date(datetimeString.replace(' ', 'T'));
+    const day = date.getDay();
+
+    const chineseDay = ['日', '一', '二', '三', '四', '五', '六'];
+    return `星期${chineseDay[day]}`;
+};
+
+const EverydayForecast = ({ weatherData }) => {
+    if(weatherData.length === 0) {
+        return <li />;
+    }
+    
+    const neededProperties = ['Wx', 'MinT', 'MaxT'];
+    const weekWeatherForecast = weatherData.reduce((neededWeatherData, weatherElement) => {
+        const propertyName = weatherElement.elementName;
+        
+        if(neededProperties.includes(propertyName)) {
+            const neededTime = weatherElement.time.filter((data, index) => {
+                return index === 0 || data.startTime.includes('06:');
+            });
+
+            neededTime.forEach((data, index) => {
+                if(index === 7) {
+                    return;
+                }
+
+                neededWeatherData[index].day = index === 0 ? '今天' : getChineseDay(data.startTime);
+                neededWeatherData[index][propertyName] = data.elementValue[0].value;
+            });
+        }
+        
+        return neededWeatherData;
+    }, Array.from({ length: 7 }, () => ({})));
+
+    return weekWeatherForecast.map((weatherForecast) => (
+        <DayForecast key={weatherForecast.day}>
+            <h5 className='flex-grow md:flex-grow-0 md:py-1 font-bold'>{weatherForecast.day}</h5>
+            <WeatherIcon weather={weatherForecast['Wx']} />
+            <p className='w-18 ml-2 md:py-1'>{weatherForecast['MinT']}° - {weatherForecast['MaxT']}°</p>
+        </DayForecast>
+    ));
+}
